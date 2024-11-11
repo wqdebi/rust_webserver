e5e4de760b977fb0bdc01067879428a7ffcdad6a
jiegeng <3136305395@qq.com>
2024-11-11 00:16:43 -0800

    day1 test

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use actix_web::{web, App, HttpRequest, HttpServer, Responder};
+use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
 async fn greet(req: HttpRequest)->impl  Responder{
     let name = req.match_info().get("name").unwrap_or("World");
     format!("Hello {}!", &name)
@@ -8,11 +8,16 @@ async fn add(req: HttpRequest)->impl  Responder{
     let b = req.match_info().get("b").unwrap_or("0").parse::<i32>().unwrap_or(0);
     format!("Hello {}!", a+b)
 }
-#[tokio::main]
+
+async fn health_check(_req: HttpRequest)->impl  Responder{
+    HttpResponse::Ok()
+}
+#[tokio::main]//过程宏
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
             .route("/", web::get().to(greet))
+            .route("/health_check", web::get().to(health_check))
             .route("/{name}", web::get().to(greet))
             .route("/{name}/{a}/{b}", web::get().to(add))
     }).bind("127.0.0.1:8080")?
