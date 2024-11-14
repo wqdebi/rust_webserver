b1adb7b70b92bc8f5e381dfd9b68e83652fba181
jiegeng <3136305395@qq.com>
2024-11-13 17:27:25 -0800

    day4

---
+++ scripts/init_db.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+set -x
+set -eo pipefail
+
+if ! [ -x "$(command -v sqlx)" ]; then
+  echo >&2 "Error: sqlx is not installed."
+  echo >&2 "Use:"
+  echo >&2 "    cargo install --version='~0.8' sqlx-cli --no-default-features --features rustls,postgres"
+  echo >&2 "to install it."
+  exit 1
+fi
+
+# Check if a custom parameter has been set, otherwise use default values
+DB_PORT="${DB_PORT:=5432}"
+SUPERUSER="${SUPERUSER:=postgres}"
+SUPERUSER_PWD="${SUPERUSER_PWD:=password}"
+APP_USER="${APP_USER:=app}"
+APP_USER_PWD="${APP_USER_PWD:=secret}"
+APP_DB_NAME="${APP_DB_NAME:=newsletter}"
+
+
+  docker run \
+      --env POSTGRES_USER=${SUPERUSER} \
+      --env POSTGRES_PASSWORD=${SUPERUSER_PWD} \
+      --health-cmd="pg_isready -U ${SUPERUSER} || exit 1" \
+      --health-interval=1s \
+      --health-timeout=5s \
+      --health-retries=5 \
+      --publish "${DB_PORT}":5432 \
+      --detach \
+      --name "${CONTAINER_NAME}" \
+      postgres -N 1000
+      # ^ Increased maximum number of connections for testing purposes
+      
+  
