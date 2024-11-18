c8c7fa148cdbed05ec6023901bcfb16152d4c1fa
jiegeng <3136305395@qq.com>
2024-11-17 22:10:45 -0800

    day7

---
+++ src/routes/subscriptions.rs
@@ -0,0 +1,10 @@
+use actix_web::{web, HttpResponse};
+
+#[derive(serde::Deserialize)]
+struct FormData{
+    email: String,
+    name: String,
+}
+async fn subscribe(_form: web::Form<FormData>)->HttpResponse {
+    HttpResponse::Ok().finish()
+}
